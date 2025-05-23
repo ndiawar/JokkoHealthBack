55153baadd4b8190a75ad9f3d24207094ac5ac4c
ndiawar <dndiawar20@gmail.com>
2025-05-23 20:41:14 +0000

    Déploiement render

--- src/middlewares/security/cors.js
+++ src/middlewares/security/cors.js
@@ -2,11 +2,15 @@ import cors from 'cors';
 
 // Configuration CORS
 const corsOptions = {
-  origin: ['http://localhost:3000'], // Autoriser l'accès uniquement depuis http://localhost:3000 (Frontend)
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Méthodes HTTP autorisées
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'], // En-têtes autorisés
-  credentials: true, // Permettre l'envoi des cookies
-  optionsSuccessStatus: 200, // Pour les anciens navigateurs qui attendent un status 200 pour les requêtes OPTIONS
+  origin: [
+    'http://localhost:3000',
+    'https://jokkohealth.onrender.com',
+    'http://localhost:3001'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Cache-Control'],
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
 
 export default cors(corsOptions);
